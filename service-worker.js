3e3e82685bbe422fff51997dda80ea44ddd1f45a
chunglinh1995 <45438751+chunglinh1995@users.noreply.github.com>
2025-11-01 18:56:51 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,8 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js').then(() => {
-    console.log('Service Worker registered');
-  });
-}
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activated");
+});
